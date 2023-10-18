3b7f6e65711bf45811667c088756319a380d22af
wu-ailsa <ailsawunyc@gmail.com>
2023-10-18 15:57:01 -0400

    added course info

--- index.js
+++ index.js
@@ -1,36 +1,104 @@
 /* OBJECTS*/
+// The provided course information.
 const CourseInfo = {
-    "id": number,
-    "name": string,
-};
-
-const AssignmentGroup = {
-    "id": number,
-    "name": string,
-    // the ID of the course the assignment group belongs to
-    "course_id": number,
-    // the percentage weight of the entire assignment group
-    "group_weight": number,
-    "assignments": [AssignmentInfo],
-};
-
-const AssignmentInfo = {
-    "id": number,
-    "name": string,
-    // the due date for the assignment
-    "due_at": Date string,
-    // the maximum points possible for the assignment
-    "points_possible": number,
-};
-
-const Learner Submission = {
-    "learner_id": number,
-    "assignment_id": number,
-    "submission": {
-      "submitted_at": Date string,
-      "score": number
+    id: 451,
+    name: "Introduction to JavaScript"
+  };
+  
+  // The provided assignment group.
+  const AssignmentGroup = {
+    id: 12345,
+    name: "Fundamentals of JavaScript",
+    course_id: 451,
+    group_weight: 25,
+    assignments: [
+      {
+        id: 1,
+        name: "Declare a Variable",
+        due_at: "2023-01-25",
+        points_possible: 50
+      },
+      {
+        id: 2,
+        name: "Write a Function",
+        due_at: "2023-02-27",
+        points_possible: 150
+      },
+      {
+        id: 3,
+        name: "Code the World",
+        due_at: "3156-11-15",
+        points_possible: 500
+      }
+    ]
+  };
+  
+  // The provided learner submission data.
+  const LearnerSubmissions = [
+    {
+      learner_id: 125,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 47
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-02-12",
+        score: 150
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 3,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 400
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-24",
+        score: 39
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-03-07",
+        score: 140
+      }
     }
-};
+  ];
+
+
+/* What you must do:
+
+Your goal is to analyze and transform this data such that the output of your program is an array of objects each containing the following information, in the following format:
+{
+    // the ID of the learner for which this data has been collected
+    "id": number,
+    // the learner’s total, weighted average, in which assignments
+    // with more points_possible should be counted for more
+    // e.g. a learner with 50/100 on one assignment and 190/200 on another
+    // would have a weighted average score of 240/300 = 80%.
+    "avg": number,
+    // each assignment should have a key with its ID,
+    // and the value associated with it should be the percentage that
+    // the learner scored on the assignment (submission.score / points_possible)
+    <assignment_id>: number,
+    // if an assignment is not yet due, it should not be included in either
+    // the average or the keyed dictionary of scores
+}
+
+*/  
+
 
 /* FUNCTIONS */
 
